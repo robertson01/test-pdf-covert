703bada60fc48370a1f1296a02b0c12e2c69d53f
robertson <kolyn9gg@gmail.com>
2018-11-25 23:29:01 +0300

    change code convert PDF

--- js/convertPFD.js
+++ js/convertPFD.js
@@ -1,13 +1,15 @@
 
-$("#pdf").click(function() {
-  html2canvas(document.querySelector('#content')).then(canvas => {
-    var a4 = [595.28, 841.89]
-    var pdf = new jsPDF('p', 'mm', 'a4');
-    pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, 211, 298);
-    pdf.save('testing.pdf');
-  });
-});
 
+  function print(quality = 1) {
+    html2canvas(document.querySelector('#content'), {
+      scale: quality
+    }).then(canvas => {
+      var a4 = [595.28, 841.89];
+      var pdf = new jsPDF('p', 'mm', 'a4');
+      pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, 211, 298);
+      pdf.save("test.pdf");
+    });
+  }
 
 
 /* slider */
